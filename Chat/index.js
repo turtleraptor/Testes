52951d32b7759c14d894ffa8a551d0f9a463045d
turtleraptor <raphaelvictal@gmail.com>
2015-08-25 08:48:00 -0300

    Update index.js

--- Chat/index.js
+++ Chat/index.js
@@ -1,3 +1,16 @@
-$(function(){
-  $("#output").append("github!");
-});
+var app = {
+    
+    initialize: function() {
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    
+    onDeviceReady: function() {
+		  $("#output").append("GIT HUB!!!");
+    }    
+};
+
+app.initialize();

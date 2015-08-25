02e30012f7206c70109d4d323de6c05ac2e79f62
turtleraptor <raphaelvictal@gmail.com>
2015-08-25 08:53:58 -0300

    Update index.js

--- Chat/index.js
+++ Chat/index.js
@@ -10,7 +10,30 @@ var app = {
     
     onDeviceReady: function() {
 		  $("#output").append("GIT HUB!!!");
+		  navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }    
 };
 
 app.initialize();
+
+// onSuccess Callback
+//   This method accepts a `Position` object, which contains
+//   the current GPS coordinates
+//
+var onSuccess = function(position) {
+    $("#output").append('Latitude: '          + position.coords.latitude          + '<br>' +
+          'Longitude: '         + position.coords.longitude         + '<br>' +
+          'Altitude: '          + position.coords.altitude          + '<br>' +
+          'Accuracy: '          + position.coords.accuracy          + '<br>' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
+          'Heading: '           + position.coords.heading           + '<br>' +
+          'Speed: '             + position.coords.speed             + '<br>' +
+          'Timestamp: '         + position.timestamp                + '<br>');
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+    $("#output").append('code: '    + error.code    + '<br>' +
+          'message: ' + error.message + '<br>');
+}

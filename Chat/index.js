68c5365aa8eac3a09a4f870b3d7d95ecdbac0a03
turtleraptor <raphaelvictal@gmail.com>
2015-08-25 09:31:21 -0300

    Update index.js

--- Chat/index.js
+++ Chat/index.js
@@ -9,38 +9,37 @@ var app = {
     },
     
     onDeviceReady: function() {
-    	
+    	// onSuccess Callback
+			//   This method accepts a `Position` object, which contains
+			//   the current GPS coordinates
+			//
+			 function onSuccess(position) {
+			    $("#output").append('Latitude: '          + position.coords.latitude          + '<br>' +
+			          'Longitude: '         + position.coords.longitude         + '<br>' +
+			          'Altitude: '          + position.coords.altitude          + '<br>' +
+			          'Accuracy: '          + position.coords.accuracy          + '<br>' +
+			          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
+			          'Heading: '           + position.coords.heading           + '<br>' +
+			          'Speed: '             + position.coords.speed             + '<br>' +
+			          'Timestamp: '         + position.timestamp                + '<br>');
+			}
+			
+			// onError Callback receives a PositionError object
+			//
+			function onError(error) {
+			    $("#output").append('code: '    + error.code    + '<br>' +
+			          'message: ' + error.message + '<br>');
+			}
+			
 		  $("#output").append("GIT HUB GPS2!!!");
 		  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		  
+		  var bt = $("<button>Refresh</button>");
+			bt.click(function(){
+				location.reload();
+			});
+			$("#output").append(bt);
     }    
 };
 
 app.initialize();
-
-var bt = $("<button>Refresh</button>");
-bt.click(function(){
-	location.reload();
-});
-$("#output").append(bt);
-
-// onSuccess Callback
-//   This method accepts a `Position` object, which contains
-//   the current GPS coordinates
-//
-var onSuccess = function(position) {
-    $("#output").append('Latitude: '          + position.coords.latitude          + '<br>' +
-          'Longitude: '         + position.coords.longitude         + '<br>' +
-          'Altitude: '          + position.coords.altitude          + '<br>' +
-          'Accuracy: '          + position.coords.accuracy          + '<br>' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '<br>' +
-          'Heading: '           + position.coords.heading           + '<br>' +
-          'Speed: '             + position.coords.speed             + '<br>' +
-          'Timestamp: '         + position.timestamp                + '<br>');
-};
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    $("#output").append('code: '    + error.code    + '<br>' +
-          'message: ' + error.message + '<br>');
-}
